ba7aa0213a94db26bf299068e1058c1898ad1cf0
ArnoldKulikov <92524587+ArnoldKulikov@users.noreply.github.com>
2022-01-17 10:55:15 +0300

    Add files via upload

---
+++ src/main/java/com/main/paymentAccounting/PaymentAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.main.paymentAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentAccountingApplication.class, args);
+	}
+
+}
